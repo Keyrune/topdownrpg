b095613b1d5c8d9fcfe1700afff22a931993ec97
Keyrune <spyro-99@mail.ru>
2021-08-01 19:28:34 +0300

    Add weapon

---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : Collidable
+{
+    // Damage
+    public int damagePoint = 1;
+    public float pushForce = 2.0f;
+
+    // Upgrade
+    public int weaponLevel = 0;
+    private SpriteRenderer spriteRenderer;
+
+    // Swing 
+    private float cooldown = 1.0f;
+    private float lastSwing = 0f;
+
+
+    protected override void Start() 
+    {
+        base.Start();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    protected override void Update()
+    {
+        base.Update();
+
+        if (Input.GetKeyDown("space"))
+        {
+            if (Time.time - lastSwing > cooldown)
+            {
+                lastSwing = Time.time;
+                Swing();
+            }
+        }
+    }
+
+    protected override void OnCollide(Collider2D coll)
+    {
+        //base.OnCollide(coll);
+    }
+
+    private void Swing()
+    {
+        Debug.Log("swing");
+    }    
+
+}
